118f4137c650c566b29d2c9ea42aef011f1af8f9
mekontso <mekontsoanderson@gmail.com>
2021-01-28 08:04:54 +0100

    Creating home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,20 @@
 <template>
-<div>Hello world</div>
+  <div>
+    <div v-for="home in homes" :key="home.objectID" style="float: left;margin: 10px">
+      <home-card :home="home"/>
+  </div>
+  </div>
 </template>
 
 <script>
+import homes from '../data/homes.json'
+import HomeCard from "~/components/HomeCard";
 export default {
-name: "index"
+  name: "index",
+  components: {HomeCard},
+  data:() =>({
+    homes:homes.slice(0,3)
+  })
 }
 </script>
 
